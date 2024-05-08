48340499185bdef4750b4a031291e87066515faf
Namukuru Lauryn <namukurulauryn@gmail.com>
2024-05-08 21:07:16 +0300

    Creation of the class

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -3,4 +3,36 @@ import 'package:user_repository/src/user_repo.dart';
 class FirebaseUserRepo implements UserRepository {
   final FirebaseAuth _firebaseAuth;
   final usersCollection = FirebaseFirestore.instance.collection('users');
+
+  FirebaseUserRepo((
+    FirebaseAuth? FirebaseAuth, 
+  )) : _firebaseAuth = FirebaseAuth ?? FirebaseAuth.instance;
+  
+  @override
+  // TODO: implement User
+  Stream<dynamic> get User => throw UnimplementedError();
+  
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> setUserData(user) {
+    // TODO: implement setUserData
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<dynamic> signUp(myUser, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
 }
